c036a35b1230cf8acaa10485865d9fcce444aa2a
slash2382 <slash2382@gmail.com>
2021-01-04 12:34:30 +0100

    added bootstrap project

---
+++ src/main/java/com/slash2382/cakeshop/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.slash2382.cakeshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String getPage(){
+        return "greeting";
+    }
+}
